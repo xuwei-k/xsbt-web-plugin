706dda4053cd23268b61b945102fae54c430fea1
Kaz Sera <seratch@gmail.com>
2016-05-22 10:09:25 +0900

    version 2.0.5

--- project/Build.scala
+++ project/Build.scala
@@ -3,13 +3,13 @@ import sbt.Keys._
 
 object SbtServletPlugin extends Build {
 
-  lazy val jettyVersion = "9.2.16.v20160414"
+  lazy val jettyVersion = "9.2.17.v20160517"
 
   lazy val root = Project(id = "sbt-servlet-plugin", base = file("."),
     settings = Seq(
       organization := "org.skinny-framework",
       name := "sbt-servlet-plugin",
-      version := "2.0.4",
+      version := "2.0.5",
       sbtPlugin := true,
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
       incOptions := sbt.inc.IncOptions.Default,
